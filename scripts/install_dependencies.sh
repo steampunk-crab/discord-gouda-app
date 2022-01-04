72f5f49192f890a288610be09ef2c157b03897fc
Joseph Caruso <93459554+steampunk-crab@users.noreply.github.com>
2022-01-04 13:23:38 -0600

    v0.1.0

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt install libglib2.0-dev libcairo-2 libatk1.0-dev libpango1.0-dev \
+    libgdk-pixbuf-2.0-dev libsoup2.4-dev libgtk-3-dev libwebkit2gtk-4.0-dev \
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use gouda::launch_web_app;
+
+fn main() {
+    launch_web_app(
+        "Discord",
+        "https://discord.com/channels/@me"
+    );
+}
